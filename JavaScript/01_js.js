95e7a28755e09c4348034b31f67b89f3351e2b03
Ankush Maurya <mauryaankush387@gmail.com>
2026-05-24 17:22:59 +0530

    Intial Commit

---
+++ JavaScript/01_js.js
@@ -0,0 +1,66 @@
+// double // for one line comments
+/*
+this is 
+multi line comments
+*/
+
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using node, alert work on browser
+
+// variable there are threee type of keyword for assigning variable
+// 1. let :- mutable
+// 2. var :- block scoop 
+// 3. const :- unmutable
+
+// const accountId = 12345 // immutable assignment(once assign does not change)
+// let accountEmail = "ankush@gmail.com" // best way to assign variables
+// var accountPassword = "1234"  // Prefer to not use because of issue in block scope and functional scope 
+// accountCity = "Prayagraj"  // in strict mode this line throws an error
+
+// console.table([accountId, accountEmail, accountPassword, accountCity]) // this use for printing data as table
+//  _______________________________
+// │ (index) │ Values             │
+// ├─────────┼────────────────────┤
+// │ 0       │ 12345              │
+// │ 1       │ 'ankush@gmail.com' │
+// │ 2       │ '1234'             │
+// │ 3       │ 'Prayagraj'        │
+// └─────────┴────────────────────┘
+
+
+// ****************DataTypes*********************
+// 1. Primitive Datatypes
+// number => 2 to power 3
+// BigInt
+// String => ""
+// Boolean => true/false
+// null => standalone Value
+// undefined 
+// symble 
+
+
+// ***************** Operations *****************
+
+// let val = 10
+// let value = -val
+// console.log(value);
+
+// console.log(3+3); // addition
+// console.log(5-2); // substraction
+// console.log(5*2); // multiplication
+// console.log(5**2); // power
+// console.log(4/2); // division
+// console.log(8%3); // remainder
+
+// ******** Concatenation
+// let str1 = "Hello"
+// let str2 = " Ankush!"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
