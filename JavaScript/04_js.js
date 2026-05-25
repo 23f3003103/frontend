221024eceedab8416bf32ca48aba4769d7c267f4
Ankush Maurya <mauryaankush387@gmail.com>
2026-05-25 16:06:44 +0530

    More on JS

--- JavaScript/04_js.js
+++ JavaScript/04_js.js
@@ -1,3 +1,26 @@
-// array
+// PRactice Question
 
-const myArr = [0, 1, 2, 3, 4, 5, 6]
+// let num = prompt("Enter a number: ")
+
+// if (num % 5 === 0) {
+//     console.log(`The  Number ${num} you entered is Multiple of 5`)
+// } else {
+//     console.log(`The Number ${num} you entered is not a multiple of 5`)
+// }
+
+
+let score = prompt("Enter Marks: ")
+
+if (score >= 90 && score <= 100) {
+    console.log("Your Grade is S")
+} else if (score >= 80 && score < 90) {
+    console.log("Your Grade is A")
+} else if (score >= 70 && score < 80) {
+    console.log("Your Grade is B")
+} else if (score >= 60 && score < 70) {
+    console.log("Your Grade is C")
+} else if (score >= 50 && score < 60) {
+    console.log("Your Grade is E")
+} else {
+    console.log("You are fail, Your Grade is F")
+}
